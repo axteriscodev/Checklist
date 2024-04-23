6933fcb2bea54e52a623076f33a2f26ec51c2894
mmichelotti89 <mmichelotti@axterisco.it>
2024-04-23 14:44:21 +0200

    componenente per il resize della view

---
+++ ClientWebApp/wwwroot/ScreenListener.js
@@ -0,0 +1,27 @@
+﻿
+/**
+ *  funzione per catturare tramite js il ridimensionamento dello schermo
+ */
+export function resizeListener(dotnethelper) {
+    $(window).resize(() => {
+        console.log("resize js");
+        let browserHeight = $(window).innerHeight();
+        let browserWidth = $(window).innerWidth();
+        // invoco il metodo di c# per settare la nuova dimensione dello schermo
+        dotnethelper.invokeMethodAsync('SetScreenDimensions', browserWidth, browserHeight).then(() => {
+        }).catch(error => {
+            console.log("Errore durante il ridimensionamento dello schermo: " + error);
+        });
+    });
+}
+
+/**
+ *  funzione per avere le dimensioni iniziali dello schermo
+ */
+export function getWindowSize() {
+    console.log("get js");
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+};
