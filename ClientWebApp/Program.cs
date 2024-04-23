eb26f332eff8f0b9dfaa37a6c5a1c186dbe06a0e
mmichelotti89 <mmichelotti@axterisco.it>
2024-04-23 12:24:44 +0200

    aggiunto libreria radzen + pagina categories nel client

--- ClientWebApp/Program.cs
+++ ClientWebApp/Program.cs
@@ -1,11 +1,15 @@
 using ClientWebApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//componenti radzen
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
