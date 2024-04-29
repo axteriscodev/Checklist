6085d7105a3093d24dad93846ec462e9494cdaa7
Giascar <giascar82@gmail.com>
2024-04-29 14:27:23 +0200

    form creation interface

--- ClientWebApp/Components/FormCreation/FormCreation.razor.cs
+++ ClientWebApp/Components/FormCreation/FormCreation.razor.cs
@@ -1,6 +1,55 @@
-﻿namespace ClientWebApp;
+﻿using ClientWebApp.Repositories;
+using Microsoft.AspNetCore.Components;
+using Shared;
 
-public abstract class FormCreation
+namespace ClientWebApp.Components.FormCreation;
+
+public partial class FormCreation
 {
+
+    private List<CategoryModel> categories = [];
+
+    private IEnumerable<int> selected = [];
+
+    /// <summary>
+    /// booleano che indica se la pagina sta eseguendo il caricamento iniziale
+    /// </summary>
+    private bool initialLoading;
+
+    /// <summary>
+    /// Booleano che è impostata durante una ricerca
+    /// </summary>
+    private bool isLoading = false;
+
+
+    /// <summary>
+    /// Intero che ci dice quanti sono gli elementi
+    /// </summary>
+    private int count;
+
+
+    [Parameter]
+    public string Param { get; set; } = "";
+
+    protected override async Task OnInitializedAsync()
+    {
+        initialLoading = true;
+        await base.OnInitializedAsync();
+        await LoadData();
+        initialLoading = false;
+    }
+
+    private async Task LoadData()
+    {
+        categories = await CategoriesRepository.GetCategories();
+        count = categories.Count;
+    }
+
+    private async Task ReloadTable()
+    {
+        //DialogService.Close();
+        await LoadData();
+        //await grid!.Reload();
+    }
 
 }
