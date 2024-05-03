118b13de4eb7dd2c61899a12cd169e2efca18723
Giascar <giascar82@gmail.com>
2024-05-03 17:14:33 +0200

    form selection dropdown

--- ClientWebApp/Components/FormCompilation/FormCompilation.razor.cs
+++ ClientWebApp/Components/FormCompilation/FormCompilation.razor.cs
@@ -14,6 +14,10 @@ public partial class FormCompilation
     /// </summary>
     private bool initialLoading;
 
+    private int CurrentSelection;
+
+    IEnumerable<int> DocumentsList = [];
+
     [Parameter]
     public string Param { get; set; } = "";
 
@@ -27,11 +31,24 @@ public partial class FormCompilation
 
     private async Task LoadData()
     {
-        documentModel = await DocumentsRepository.GetDocumentById(1);
+        List<DocumentModel> docLists = await DocumentsRepository.GetDocuments();
+
+        if (docLists.Count != 0)
+        {
+            DocumentsList = docLists.Select(x => x.Id);
+            CurrentSelection = docLists.First().Id;
+
+            documentModel = await DocumentsRepository.GetDocumentById(CurrentSelection);
+        }
+    }
+
+    private async Task OnDocumentSelected()
+    {
+        documentModel = await DocumentsRepository.GetDocumentById(CurrentSelection);
     }
 
     private async Task SaveForm()
     {
-       DocumentsRepository.UpdateDocuments([documentModel]);
+        DocumentsRepository.UpdateDocuments([documentModel]);
     }
 }
--- ClientWebApp/Repositories/DocumentsRepository.cs
+++ ClientWebApp/Repositories/DocumentsRepository.cs
@@ -14,7 +14,7 @@ public class DocumentsRepository(HttpManager httpManager)
     {
         if (Documents.Count == 0)
         {
-            var response = await _httpManager.SendHttpRequest("Document/DocumentsList", "");
+            var response = await _httpManager.SendHttpRequest("Document/DocumentsList", 0);
             if (response.Code.Equals("0"))
             {
                 Documents = JsonSerializer.Deserialize<List<DocumentModel>>(response.Content.ToString() ?? "") ?? [];
