706ff8433fae97bcd085347e9e93f8d85be50072
Giascar <giascar82@gmail.com>
2024-06-11 12:34:13 +0200

    TemplateDescriptionSelectionStep

---
+++ ConstructionSiteLibrary/Components/Templates/Wizard/TemplateDescriptionSelectionStep.razor.cs
@@ -0,0 +1,31 @@
+﻿using ConstructionSiteLibrary.Components.Utilities;
+using Shared.Templates;
+
+namespace ConstructionSiteLibrary.Components.Templates.Wizard;
+
+public partial class TemplateDescriptionSelectionStep
+{
+    List<TemplateDescriptionModel> TemplatesDescriptions = [];
+
+    TemplateDescriptionModel? CurrentSelection;
+
+    /// <summary>
+    /// booleano che indica se la pagina sta eseguendo il caricamento iniziale
+    /// </summary>
+    private bool initialLoading;
+
+    ScreenComponent screenComponent;
+
+    protected override async Task OnInitializedAsync()
+    {
+        initialLoading = true;
+        await base.OnInitializedAsync();
+        await LoadData();
+        initialLoading = false;
+    }
+
+    private async Task LoadData()
+    {
+        TemplatesDescriptions = await TemplatesRepository.GetTemplatesDescriptions();
+    }
+}
