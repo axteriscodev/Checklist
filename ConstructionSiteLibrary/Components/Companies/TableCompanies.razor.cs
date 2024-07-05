71740f6c9483c10509e5c0b88ab295f73c5dcf64
Giascar <giascar82@gmail.com>
2024-07-05 16:41:45 +0200

    tabella aziende

---
+++ ConstructionSiteLibrary/Components/Companies/TableCompanies.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using ConstructionSiteLibrary.Components.Utilities;
+using Radzen.Blazor;
+using Shared.Documents;
+
+namespace ConstructionSiteLibrary.Components.Companies;
+
+public partial class TableCompanies
+{
+    private List<CompanyModel> companies = [];
+
+    private bool initialLoading;
+
+    private bool isLoading = false;
+
+    private int count;
+
+    private int pageSize = 8;
+
+    private string pagingSummaryFormat = "Pagina {0} di {1} (Totale {2} aziende)";
+
+    private RadzenDataGrid<CompanyModel>? grid;
+
+    [Parameter]
+    public string Param { get; set; } = "";
+
+    ScreenComponent screenComponent;
+
+    protected override async Task OnInitializedAsync()
+    {
+        initialLoading = true;
+        await base.OnInitializedAsync();
+        await LoadData();
+        initialLoading = false;
+    }
+
+    private async Task LoadData()
+    {
+        companies = await CompaniesRepository.GetCompanies();
+        count = companies.Count;
+    }
+
+    private async Task ReloadTable()
+    {
+        await LoadData();
+        await grid.Reload();
+    }
+}
