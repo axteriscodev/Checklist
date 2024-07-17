cc905b65e4a5a4ccd8e68af90363e1ac72d9433c
Giascar <giascar82@gmail.com>
2024-07-17 11:51:25 +0200

    aggiunto splitbutton

--- ClientWebApp/Services/CameraService.cs
+++ ClientWebApp/Services/CameraService.cs
@@ -3,19 +3,33 @@ using ConstructionSiteLibrary.Components.Utilities;
 using ConstructionSiteLibrary.Interfaces;
 using ClientWebApp.Components;
 using Radzen;
+using ConstructionSiteLibrary.Services;
+using ConstructionSiteLibrary.Managers;
+using Microsoft.JSInterop;
 
 
 namespace ClientWebApp.Services
 {
-    public class CameraService(DialogService dialogService) : ICameraService
+    public class CameraService(DialogService dialogService, IJSRuntime IJSRuntime) : ICameraService
     {
 
         private DialogService _dialogService = dialogService;
 
+        private IJSRuntime _iJSRuntime = IJSRuntime;
+
         public async Task<string> OpenCamera()
         {
-           
-            var img = await _dialogService.OpenAsync<CameraComponent>("");
+            var screenManager = new ScreenManager();
+            await screenManager.Init(_iJSRuntime);
+            var screenSize = screenManager.GetScreenSize();
+
+            var scritta =  $"width : {screenSize!.Width}px; height: {screenSize!.Height}px;";
+
+            var options = new DialogOptions
+            {
+                Style = $"width : {screenSize!.Width}px; height: {screenSize!.Height}px;",
+            };
+            var img = await _dialogService.OpenAsync<CameraComponent>("", options: options);
             return img;
         }
     }
