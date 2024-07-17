02e3e25cf2730c25dee18b7cda0e60033de18d0f
mmichelotti89 <mmichelotti@axterisco.it>
2024-07-17 18:24:47 +0200

    modifiche alla gestione della navbar in base alla grandezza dello schermo

--- ConstructionSiteLibrary/Utility/GlobalVariables.cs
+++ ConstructionSiteLibrary/Utility/GlobalVariables.cs
@@ -7,14 +7,12 @@ namespace ConstructionSiteLibrary.Utility
     public class GlobalVariables
     {
 
-        public const string MinWidthNavbat = "641px";
+        public const int MinNavbarPx = 641;
         public const string NavbarWidthOpen = "250px";
         public const string NavbarWidthClosed = "80px";
         public const Variant ComponentVariant = Variant.Outlined;
         public const int PageSize = 8;
 
-
-        public NavbarState IsNavBarOpen { get; set; } = NavbarState.Open;
 
 
     }
