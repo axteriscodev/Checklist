5daaa0c45955b7241a6e69eb704a6ec1c7566df0
mmichelotti89 <mmichelotti@axterisco.it>
2025-12-02 18:28:23 +0100

    correzione template description

--- TDatabase/Queries/TemplateDbHelper.cs
+++ TDatabase/Queries/TemplateDbHelper.cs
@@ -73,7 +73,6 @@ public class TemplateDbHelper
                                                              }).ToList()
                                                        }).ToList()
                                       }).ToList(),
-
                     }).ToList();
 
         return docs;
